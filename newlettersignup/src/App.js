f7a03218b5404d2fcbca908df973ac67e768d26b
TedPasquiet <tpasquiet@hotmail.fr>
2024-01-25 22:19:47 +0100

    ecred

--- newlettersignup/src/App.js
+++ newlettersignup/src/App.js
@@ -27,7 +27,7 @@ function App() {
         <p>
           {" "}
           A confirmation email has been sent to ash@loremcompany com. Please
-          open it and click the button inside to confirm your subscription.
+          open  it and click the button inside to confirm your subscription.
         </p>
         <button class="Button">Dismiss message</button>
       </div>
